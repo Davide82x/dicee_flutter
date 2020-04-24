2162438d812baf0adfbe5a3a4b6e9b1e0038aaee
Davide Vanoni <davide.vanoni@gmail.com>
2020-04-24 15:43:50 +0200

    Added statuful widget and dynamic change of the dices.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -15,23 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   // This widget is the root of your application.
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomizeDice(){
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
+  //The Hot Reload works inside build
+  //All the changes of the state is evaluated inside build
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  randomizeDice();
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  randomizeDice();
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
@@ -39,3 +63,6 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+
+
