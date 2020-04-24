ad0594b7f328ae532db06b707728b5b578f0879f
dvanonihupac <dvanoni@hupac.com>
2020-04-24 11:40:37 +0200

    UI completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,23 @@ class DicePage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
